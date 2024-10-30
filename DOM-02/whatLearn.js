8e3c97fa5ae40dcf7ce82e8f6608e5837b6c3462
anujkp-97 <anujpal9770@gmail.com>
2024-10-30 23:02:41 +0530

    DOM

---
+++ DOM-02/whatLearn.js
@@ -0,0 +1,66 @@
+/*
+    What we will learn 
+
+    1. What is DOM?
+
+    2. What is nodes and types of node.
+
+    3. Selection element using DOM
+        
+        a. getElementById()
+        b. getElementByName()
+        c. getElementByTagName()
+        d. getElementByClassName()
+        e. querySelector() & querySelectorAll()
+
+    4. Traversing Element
+
+        a. Selecting parent element
+        b. Selecting Child element
+        c. Selecting Sibling element
+
+    5. Manupulating HTML elements
+
+        a. createElement()
+        b. appendChild()
+        c. textContent()
+        d. innerHTML
+        e. append()
+        f. prepend()
+        g. insertAdjacentElement()
+        h. replaceChild()
+        i. cloneNode()
+        j. removeChild()
+        k. insertBefore()
+
+    6. Attribute methods
+
+        a. getAttribute()
+        b. setAttribute()
+        c. hasAttribute()
+        d. removeAttribute()
+
+    7. Manipulating Element's Styles
+
+        a. style property
+        b. cssText
+        c. getComputedStyle()
+        d. className property
+        e. classList property
+
+    8. JavaScript Events
+
+        a. What is event? 
+        b. Event bubbling and event capturing
+        c. Event Handler in HTML Attributes
+        d. DOM level 0 event handlers
+        e. addEventListener()
+        f. removeEventListener()
+        g. Event Objects
+        h. Different Types of Event
+
+
+
+
+
+*/
